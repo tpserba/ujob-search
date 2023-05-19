5c1e3bba8836e501d05c4e6943e282a6fe8730e8
Tomás Praga <tpraga@serbatic.es>
2023-05-19 14:06:20 +0200

    create MainNav component and render it

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1 class="mt-8 text-center text-6xl">Hello, I'm in App.vue</h1>
+  <MainNav />
 </template>
+
+<script>
+import MainNav from '@/components/MainNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainNav
+  }
+}
+</script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="w-full text-sm">Main nav</header>
+</template>
+
+<script>
+export default {
+  name: 'MainNav'
+}
+</script>
