5221c72d456f75749d4872a65a7bda9fc767e0bf
Tomás Praga <tpraga@serbatic.es>
2023-05-19 12:18:58 +0200

    apply global style

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1>Hello, I'm in App.vue</h1>
   <div class="wrapper">
     <HelloWorld msg="You did it!" />
   </div>
 </template>
+
+<style>
+h1 {
+  color: purple;
+}
+</style>
