73c2f6b615c9460e56606e621489456b0268a8df
Tomás Praga <tpraga@serbatic.es>
2023-05-22 16:41:46 +0200

    add custom shadow to tailwind and style button

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white">Sign in</button>
+  <button
+    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
+  >
+    Sign in
+  </button>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'brand-grey-1': '#dadce0',
         'brand-blue-1': '#1967d2',
         'brand-green-1': '#137333'
+      },
+      boxShadow: {
+        blue: '0 0 3px 3px #4285f4'
       }
     }
   },
