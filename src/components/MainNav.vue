15dee4eee077114b8d3d4fba70c951fda1b50240
Tomás Praga <tpraga@serbatic.es>
2023-05-22 08:24:59 +0200

    render data to view

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-grey-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">Corp Careers</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -10,7 +10,12 @@
 
 <script>
 export default {
-  name: 'MainNav'
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Corp Careers'
+    }
+  }
 }
 </script>
 
