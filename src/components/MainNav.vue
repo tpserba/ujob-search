6793accfb471a880ff4a226666bb13689c2ed09d
Tomás Praga <tpraga@serbatic.es>
2023-05-22 15:23:37 +0200

    add key to each item in the nav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,7 +5,7 @@
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li class="ml-9 h-full" v-for="menuItem in menuItems">
+            <li class="ml-9 h-full" v-for="menuItem in menuItems" :key="menuItem">
               <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
             </li>
           </ul>
