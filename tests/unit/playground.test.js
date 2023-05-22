f7efab4e6468b39682c2134f090cfec46e561ee5
Tomás Praga <tpraga@serbatic.es>
2023-05-22 10:06:27 +0200

    display vitest syntax and show multiple describe blocks

--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,7 +1,21 @@
 import { describe, it, expect } from 'vitest'
+import { evenOrOdd } from '@/playground.js'
 
 describe('basic math', () => {
   it('adds two numbers', () => {
     expect(1 + 1).toBe(2)
+  })
+
+  describe('Even or odd', () => {
+    describe('When numner is even', () => {
+      it('indicates if the numnber is even', () => {
+        expect(evenOrOdd(4)).toBe('Even')
+      })
+    })
+  })
+  describe('Even or odd', () => {
+    it('indicates if the numnber is odd', () => {
+      expect(evenOrOdd(3)).toBe('Odd')
+    })
   })
 })
