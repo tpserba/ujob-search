75ba759769e89f5743897cdaefe74f191d8da865
Tomás Praga <tpraga@serbatic.es>
2023-05-22 15:46:40 +0200

    test v-for directive

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,14 @@ describe('MainNavm', () => {
     const companyName = screen.getByText('Corp Careers')
     expect(companyName).toBeInTheDocument()
   })
+
+  it('Displays menu items for navigation', () => {
+    render(MainNav)
+    const navItems = screen.getAllByRole('listitem')
+    console.log(navItems)
+    const navMenuTexts = navItems.map((item) => item.textContent)
+    console.log(navMenuTexts)
+
+    expect(navMenuTexts).toEqual(['Teams', 'Locations', 'Life at Corp', 'Students', 'Jobs'])
+  })
 })
