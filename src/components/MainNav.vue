28f3cc63d84aac7ccb87bf5b922b41f20f12bcb8
Tomás Praga <tpraga@serbatic.es>
2023-05-22 10:56:53 +0200

    code cleanup

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,9 +3,6 @@
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-grey-1 px-8">
         <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">
-          Developed by {{ author.firstName }} {{ author.lastName }}
-        </h2>
       </div>
     </div>
   </header>
@@ -16,11 +13,7 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Corp Careers',
-      author: {
-        firstName: 'John',
-        lastName: 'Doe'
-      }
+      company: 'Corp Careers'
     }
   }
 }
