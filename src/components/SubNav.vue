42f0e6d898af0abd9a8e1204e029bf04525cde51
Tomás Praga <tpraga@serbatic.es>
2023-05-23 12:14:19 +0200

    add style to subnav component

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>This is a subnav</div>
+  <div class="h-16 w-full border-b border-solid border-brand-grey-1 bg-white">
+    <div class="flex h-full items-center px-8">This is a subnav</div>
+  </div>
 </template>
 
 <script>
