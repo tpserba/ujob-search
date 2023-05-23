670caa9fe0db9b5e5f2a84c241b796f0d7ca4537
Tomás Praga <tpraga@serbatic.es>
2023-05-23 12:27:03 +0200

    register global fontawesome component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 //import './assets/main.css'
 
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import '@/index.css'
 import App from './App.vue'
+
+library.add(faSearch)
 
 createApp(App).mount('#app')
