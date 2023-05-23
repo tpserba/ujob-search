5935c8767bca44666e23ba1182f2f3f4006c43df
Tomás Praga <tpraga@serbatic.es>
2023-05-23 16:04:42 +0200

    Create headline component and demonstrate lifecycle hook access to data properties

---
+++ src/components/Headline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next job at Corp</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Headline',
+  data() {
+    return {
+      sample: 'Hello'
+    }
+  },
+  created() {
+    console.log('Creating, greeting now: ' + this.sample)
+  }
+}
+</script>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,9 @@
     <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1 border border-blue-700">1 column</div>
-        <div class="col-span-5 col-start-2 border border-blue-700">5 column</div>
+        <div class="col-span-5 col-start-2 border border-blue-700">
+          <Headline />
+        </div>
         <div class="col-span-5 col-start-7 border border-blue-700">5 column</div>
         <div class="col-span-1 col-start-12 border border-blue-700">1 column</div>
       </div>
@@ -12,16 +14,11 @@
 </template>
 
 <script>
+import Headline from '@/components/Headline.vue'
 export default {
   name: 'Hero',
-  beforeCreate() {
-    console.log('Component is about to be created')
-  },
-  created() {
-    console.log('Component has been craeted')
-  },
-  mounted() {
-    console.log('Component has been mounted in the DOM')
+  components: {
+    Headline
   }
 }
 </script>
