902ff082d41bff7b615f9a4dbbd572d8f0bb1fbf
Tomás Praga <tpraga@serbatic.es>
2023-05-23 08:32:52 +0200

    implement event listener to button with v-on directive

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
+    v-on:click="handleClick"
   >
     Sign in
   </button>
@@ -8,6 +9,11 @@
 
 <script>
 export default {
-  name: 'ActionButton'
+  name: 'ActionButton',
+  methods: {
+    handleClick() {
+      console.log("I've been clicked")
+    }
+  }
 }
 </script>
