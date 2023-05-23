19401905f76f736a5e1d18541a1137367b60bb14
Tomás Praga <tpraga@serbatic.es>
2023-05-23 13:59:52 +0200

    dynamically adjust hero component height

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-grey-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
