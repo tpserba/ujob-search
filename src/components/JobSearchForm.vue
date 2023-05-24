fabdbbb604e7ab95906818d00282211dd181ea73
Tomás Praga <tpraga@serbatic.es>
2023-05-24 09:32:07 +0200

    add more style to jobsearchform compo

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,11 +1,38 @@
 <template>
   <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-grey-3">
     <FontAwesomeIcon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light">
+      <div class="relative flex h-full flex-1 items-center pr-3">
+        <label class="absolute -top-10 left-0">Role</label>
+        <input
+          type="text"
+          placeholder="Software engineer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+      <span
+        class="flex h-full items-center border-l border-r border-brand-grey-3 bg-brand-grey-2 px-3"
+        >in</span
+      >
+      <div class="relative flex h-full flex-1 items-center pl-3">
+        <label class="absolute -top-10 left-0">Where?</label>
+        <input
+          type="text"
+          placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+    </div>
+    <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
   </form>
 </template>
 
 <script>
+import ActionButton from '@/components/ActionButton.vue'
 export default {
-  name: 'JobSearchForm'
+  name: 'JobSearchForm',
+  components: {
+    ActionButton
+  }
 }
 </script>
