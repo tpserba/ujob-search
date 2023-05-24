8ba39f0248e7a4eada1ec34c01ffab780d25c95b
Tomás Praga <tpraga@serbatic.es>
2023-05-24 10:15:31 +0200

    add vue img in grid

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,7 +7,12 @@
           <Headline />
           <JobSearchForm />
         </div>
-        <div class="col-span-5 col-start-7"></div>
+        <div class="col-span-5 col-start-7 self-center justify-self-center">
+          <img
+            class="h-80 w-80 object-contain"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+          />
+        </div>
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
