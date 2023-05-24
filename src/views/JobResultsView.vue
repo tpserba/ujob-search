48a51db97253b5bb1b2892cf11e5621db32f60f4
Tomás Praga <tpraga@serbatic.es>
2023-05-24 13:14:58 +0200

    add navigate to home

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,16 @@
 <template>
   <h1 class="text-6xl font-bold">Job results page</h1>
+  <button @click="goToHome">Go to home page</button>
 </template>
 
 <script>
 export default {
-  name: 'JobResultsView'
+  name: 'JobResultsView',
+  methods: {
+    goToHome() {
+      // Can use path or name, but name could be argued that it's less likely to change than a path
+      this.$router.push({ name: 'Home' })
+    }
+  }
 }
 </script>
