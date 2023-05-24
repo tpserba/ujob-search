1c819191e2a21242eaf01effcddb69857e72d1d9
Tomás Praga <tpraga@serbatic.es>
2023-05-24 09:18:57 +0200

    add style to jobsearchform compo

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,5 +1,7 @@
 <template>
-  <form>Lorem</form>
+  <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-grey-3">
+    <FontAwesomeIcon :icon="['fas', 'search']" class="ml-4 mr-3" />
+  </form>
 </template>
 
 <script>
