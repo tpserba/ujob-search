73c1d6e6742c3bb5353e09a1f2241887a5a9520a
Tomás Praga <tpraga@serbatic.es>
2023-05-24 16:33:00 +0200

    fix test code

--- tests/unit/components/navigation/SubNav.test.js
+++ tests/unit/components/navigation/SubNav.test.js
@@ -2,50 +2,38 @@ import { render, screen } from '@testing-library/vue'
 import SubNav from '@/components/navigation/SubNav.vue'
 
 describe('SubNav', () => {
+  const renderSubNav = (routeName) => {
+    render(SubNav, {
+      global: {
+        mocks: {
+          // Replaces this.$route with our mock
+          $route: {
+            name: routeName
+          } // could be simply $route since name is the same, left for showcase
+        },
+        stubs: {
+          FontAwesomeIcon: true
+        }
+      }
+    })
+  }
   describe('When user is on jobs page', () => {
     it('Displays job count', () => {
-      const $route = {
-        name: 'JobsResult'
-      }
-      render(SubNav, {
-        // Needs to mock that the user is on the jobs results page
-        // since the value is no longer hard coded and stored in a property and is computed now
-        computed: {
-          onJobsResultsPage() {
-            return true
-          }
-        },
-        global: {
-          mocks: {
-            // Replaces this.$route with our mock
-            $route: $route // could be simply $route since name is the same, left for showcase
-          },
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        }
-      })
+      const routeName = 'JobsResults'
+      renderSubNav(routeName)
+
       const jobCount = screen.getByText('1653')
+      console.log(jobCount)
       expect(jobCount).toBeInTheDocument()
     })
   })
 
   describe('When user is not on jobs page', () => {
     it('Does not display job count', () => {
-      const $route = {
-        name: 'Home'
-      }
-      render(SubNav, {
-        global: {
-          mocks: {
-            $route
-          },
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        }
-      })
+      const routeName = 'Home'
+      renderSubNav(routeName)
       const jobCount = screen.queryByText('1653')
+      console.log(jobCount)
       expect(jobCount).not.toBeInTheDocument()
     })
   })
