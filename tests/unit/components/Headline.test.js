0c2d104c6b3dca30d10d2aa7a05e52fd0547ab00
Tomás Praga <tpraga@serbatic.es>
2023-05-24 08:45:09 +0200

    add test to check clearInterval is called

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -1,4 +1,4 @@
-import nextElementInList from '@/utils/NextElementInList'
+import { nextTick } from 'vue'
 import Headline from '@/components/Headline.vue'
 import { render, screen } from '@testing-library/vue'
 import { vi } from 'vitest'
@@ -24,8 +24,31 @@ describe('Headline', () => {
     const mockFunc = vi.fn()
     vi.stubGlobal('setInterval', mockFunc)
     render(Headline)
-
     expect(mockFunc).toHaveBeenCalled()
     vi.useRealTimers()
+  })
+
+  it('Swaps action verb after interval', async () => {
+    vi.useFakeTimers()
+    render(Headline)
+    vi.advanceTimersToNextTimer()
+    await nextTick()
+    const actionPhrase = screen.getByRole('heading', {
+      name: /create for everyone/i
+    })
+    expect(actionPhrase).toBeInTheDocument()
+    vi.useRealTimers()
+  })
+
+  it('Removes interval when component disappears', () => {
+    vi.useFakeTimers()
+    const clearInterval = vi.fn()
+    vi.stubGlobal('clearInterval', clearInterval)
+    const { unmount } = render(Headline)
+    unmount()
+
+    expect(clearInterval).toHaveBeenCalled()
+    vi.useRealTimers()
+    vi.unstubAllGlobals()
   })
 })
