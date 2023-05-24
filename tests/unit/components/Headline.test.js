fb7795537ffe52e26dd6a44786239d06f58c1cc0
Tomás Praga <tpraga@serbatic.es>
2023-05-24 08:30:26 +0200

    add headline tests

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -1,12 +1,31 @@
 import nextElementInList from '@/utils/NextElementInList'
 import Headline from '@/components/Headline.vue'
+import { render, screen } from '@testing-library/vue'
+import { vi } from 'vitest'
 
 describe('Headline', () => {
   describe('Vitest playground', () => {
-    it('Tracks whether it has been called', () => {
-      const mockFunc = vi.fn()
-      mockFunc(1, 2)
-      expect(mockFunc).toHaveBeenCalledWith(1, 2)
+    it('Displays introductory action verb', () => {
+      // Replaces time related functions with mock functions
+      vi.useFakeTimers()
+      render(Headline)
+
+      const actionPhrase = screen.getByRole('heading', {
+        name: /build for everyone/i
+      })
+      expect(actionPhrase).toBeInTheDocument()
+      // Reverts back to normal
+      vi.useRealTimers()
     })
+  })
+
+  it('Changes action verb at a consistent interval', () => {
+    vi.useFakeTimers()
+    const mockFunc = vi.fn()
+    vi.stubGlobal('setInterval', mockFunc)
+    render(Headline)
+
+    expect(mockFunc).toHaveBeenCalled()
+    vi.useRealTimers()
   })
 })
