d339c5a1158c5b598bf3203b882ddc12177ff33c
Tomás Praga <tpraga@serbatic.es>
2023-05-25 13:56:53 +0200

    fix tests and refactor

--- tests/unit/components/job-results/JobListings.test.js
+++ tests/unit/components/job-results/JobListings.test.js
@@ -7,21 +7,37 @@ import { RouterLinkStub } from '@vue/test-utils'
 vi.mock('axios')
 
 describe('JobListings', () => {
-  it('fetches jobs', () => {
-    axios.get.mockResolvedValue({ data: [] })
-    render(JobListings)
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs') // the call is what matters, it doesn't matter is response.data is empty array
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: '5',
+      ...queryParams
+    }
   })
 
-  it('displays maximum of 10 jobs', async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
+        mocks: {
+          $route
+        },
         stubs: {
           RouterLink: RouterLinkStub
         }
       }
     })
+  }
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] })
+    const $route = createRoute()
+    renderJobListings($route)
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs') // the call is what matters, it doesn't matter is response.data is empty array
+  })
+
+  it('displays maximum of 10 jobs', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    const queryParams = { page: '1' }
+    const $route = createRoute(queryParams)
+    renderJobListings($route)
     const jobListings = await screen.findAllByRole('listitem')
     expect(jobListings).toHaveLength(10)
   })
