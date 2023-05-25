b83c87b023b8e5a0a0e2f8f9eacc4e41a945e3ce
Tomás Praga <tpraga@serbatic.es>
2023-05-25 12:28:37 +0200

    create joblistings compo tests

---
+++ tests/unit/components/job-results/JobListings.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/vue'
+import JobListings from '@/components/job-results/JobListings.vue'
+import axios from 'axios'
+import { RouterLinkStub } from '@vue/test-utils'
+
+// Finds axios library and replaces exports with vitest mock functions
+vi.mock('axios')
+
+describe('JobListings', () => {
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] })
+    render(JobListings)
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs') // the call is what matters, it doesn't matter is response.data is empty array
+  })
+
+  it('creates a job listing for every job', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    render(JobListings, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const jobListings = await screen.findAllByRole('listitem')
+    expect(jobListings).toHaveLength(15)
+  })
+})
