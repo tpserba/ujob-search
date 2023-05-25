b13b76173603c4373f779db269a17b59f92dae8a
Tomás Praga <tpraga@serbatic.es>
2023-05-25 10:33:13 +0200

    substitute hardcoded data with dynamic obtained from be

--- src/components/job-results/JobListing.vue
+++ src/components/job-results/JobListing.vue
@@ -5,12 +5,18 @@
       class="rounder hover:shadow-grey mx-auto block border border-solid border-brand-grey-2 bg-white"
     >
       <div class="mx-8 border-b border-solid border-brand-grey-2 pb-2 pt-5">
-        <h2 class="mb-2 text-2xl">Project lead</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Corp</span>
+            <span>{{ job.organization }}</span>
           </div>
-          <div><span>San Francisco, CA, USA</span></div>
+          <div>
+            <ul>
+              <li v-for="location in job.locations" :key="location" class="mr-5 inline-block">
+                <span>{{ location }}</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div class="px-8 py-4">
@@ -18,11 +24,8 @@
           <h3 class="mb-2 mt-1">Qualifications</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's degree or equivalent</li>
-              <li>5 years or more in program management</li>
-              <li>
-                Experience analyzing data through querying database(e.g. SQL). Lorem ipsum lorem
-                ipsum lorem ipsum
+              <li v-for="qualification in job.minimumQualifications" :key="qualification">
+                {{ qualification }}
               </li>
             </ul>
           </div>
