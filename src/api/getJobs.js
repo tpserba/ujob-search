c6122aabcfe448bc903ccd44a81cb4fa443d803a
Tomás Praga <tpraga@serbatic.es>
2023-05-26 13:14:18 +0200

    test that data is extracted after request

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const getJobs = async () => {
   const baseURL = import.meta.env.VITE_APP_API_URL
   const url = `${baseURL}/jobs`
-  await axios.get(url)
+  const response = await axios.get(url)
+  return response.data
 }
 
 export default getJobs
--- tests/unit/api/GetJobs.test.js
+++ tests/unit/api/GetJobs.test.js
@@ -3,8 +3,23 @@ import getJobs from '@/api/getJobs.js'
 
 vi.mock('axios')
 describe('getJobs', () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: 'Java Engineer'
+        }
+      ]
+    })
+  })
   it('fetches jobs that candidates can apply to', async () => {
     await getJobs()
     expect(axios.get).toHaveBeenCalledWith('http://myfakeapi.com/jobs')
+  })
+  it('extracts jobs from response', async () => {
+    const jobs = await getJobs()
+    const expected = [{ id: 1, title: 'Java Engineer' }]
+    expect(jobs).toEqual(expected)
   })
 })
