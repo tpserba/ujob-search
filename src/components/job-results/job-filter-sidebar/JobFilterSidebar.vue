774e09e82add7d15873452e4fda6d0815d0bea6f
Tomás Praga <tpraga@serbatic.es>
2023-05-26 15:18:10 +0200

    add style to sidebar

--- src/components/job-results/job-filter-sidebar/JobFilterSidebar.vue
+++ src/components/job-results/job-filter-sidebar/JobFilterSidebar.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="flex w-96 flex-col border-r border-solid border-brand-grey-1 bg-white p-4">
-    Job filters sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <ActionButton text="Clear filter" type="secondary" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from '@/components/shared/ActionButton.vue'
 export default {
-  name: 'JobFilterSidebar'
+  name: 'JobFilterSidebar',
+  components: {
+    ActionButton
+  }
 }
 </script>
