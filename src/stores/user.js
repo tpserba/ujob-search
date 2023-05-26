aa90f6fcae74a98e0c9fd5c4f66e75b87ac0d912
Tomás Praga <tpraga@serbatic.es>
2023-05-26 11:19:37 +0200

    add store action to be able to change data

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,5 +3,10 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     isLoggedIn: false
-  })
+  }),
+  actions: {
+    loginUser() {
+      this.isLoggedIn = true
+    }
+  }
 })
