97ef6ec150279e4a9fbf93503d9d181af2d42980
Tomás Praga <tpraga@serbatic.es>
2023-05-26 16:20:39 +0200

    refactor test code

--- tests/unit/components/shared/CollapsibleAccordion.test.js
+++ tests/unit/components/shared/CollapsibleAccordion.test.js
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event'
 import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue'
 
 describe('CollapsibleAccordion', () => {
-  it('renders child content', async () => {
+  const renderCollapsibleAccordion = (config = {}) => {
     render(CollapsibleAccordion, {
       global: {
         stubs: {
@@ -15,8 +15,20 @@ describe('CollapsibleAccordion', () => {
       },
       slots: {
         default: '<h3>My nested child</h3>'
-      }
+      },
+      ...config
     })
+  }
+
+  it('renders child content', async () => {
+    const props = {
+      header: 'My category'
+    }
+    const slots = {
+      default: '<h3>My nested child</h3>'
+    }
+    const config = { props, slots }
+    renderCollapsibleAccordion(config)
     // It doesn't expect it to be in the document
     // since starting value of the accordion is false(closed) and nothing (so no child content) is displayed
     expect(screen.queryByText('My nested child')).not.toBeInTheDocument()
@@ -29,16 +41,12 @@ describe('CollapsibleAccordion', () => {
 
   describe('when parent does not provide content for child', () => {
     it('renders fallback content', async () => {
-      render(CollapsibleAccordion, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        },
-        props: {
-          header: 'My category'
-        }
-      })
+      const props = {
+        header: 'My category'
+      }
+      const slots = {}
+      const config = { props, slots }
+      renderCollapsibleAccordion(config)
       const button = screen.getByRole('button', { name: /my category/i })
       await userEvent.click(button)
       expect(screen.getByText('Fallback content')).toBeInTheDocument()
