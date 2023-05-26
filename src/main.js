23ecc471a089b2030532d2e5dfe0e3f3ab97366c
Tomás Praga <tpraga@serbatic.es>
2023-05-26 15:20:22 +0200

    add font awesome icons to app

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import { createApp } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faAngleUp, faSearch } from '@fortawesome/free-solid-svg-icons'
 import router from '@/router'
 import { createPinia } from 'pinia'
 import '@/index.css'
 import App from './App.vue'
 
 library.add(faSearch)
+library.add(faAngleDown)
+library.add(faAngleUp)
 const pinia = createPinia()
 
 createApp(App).use(pinia).use(router).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
