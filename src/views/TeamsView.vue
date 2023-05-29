e4e174011c82e4350f643683a4cf6cf28cf0303c
Tomás Praga <tpraga@serbatic.es>
2023-05-29 08:45:27 +0200

    implement named slots

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,13 @@
 <template>
-  <HeaderContainer />
+  <HeaderContainer>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+
+    <template v-slot:subtitle>
+      <h2 class="my-4 w-full text-base font-light">Subtitle for Teams</h2>
+    </template>
+  </HeaderContainer>
 </template>
 
 <script>
