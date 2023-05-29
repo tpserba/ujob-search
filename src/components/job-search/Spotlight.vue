70ea461d95c7d0b0e8138e59a7be75cf92e42b86
Tomás Praga <tpraga@serbatic.es>
2023-05-29 09:35:37 +0200

    create spotlight component

---
+++ src/components/job-search/Spotlight.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot :spotlight="spotlight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Spotlight',
+  data() {
+    return {
+      spotlights: []
+    }
+  },
+  async mounted() {
+    const baseURL = import.meta.env.VITE_APP_API_URL
+    const url = `${baseURL}/spotlights`
+    const response = await axios.get(url)
+    this.spotlights = response.data
+  }
+}
+</script>
