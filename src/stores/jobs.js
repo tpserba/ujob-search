fcd12c4fda047b8b40369aee7d8da24327316bd8
Tomás Praga <tpraga@serbatic.es>
2023-05-29 11:05:49 +0200

    create pinia getter

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import getJobs from '@/api/getJobs.js'
 export const FETCH_JOBS = 'FETCH_JOBS'
+export const UNIQUE_ORGS = 'UNIQUE_ORGS'
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
     jobs: []
@@ -8,6 +9,15 @@ export const useJobsStore = defineStore('jobs', {
   actions: {
     async [FETCH_JOBS]() {
       this.jobs = await getJobs()
+    }
+  },
+  getters: {
+    [UNIQUE_ORGS](state) {
+      const uniqueOrgs = new Set()
+      state.jobs.forEach((job) => {
+        uniqueOrgs.add(job.organization)
+        return uniqueOrgs
+      })
     }
   }
 })
