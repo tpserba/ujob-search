8f215a088d036c5dfa3bc9b1eef01279bbee3592
Tomás Praga <tpraga@serbatic.es>
2023-05-29 12:50:46 +0200

    store user checkbox choices

--- src/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.vue
@@ -4,7 +4,7 @@
       <fieldset>
         <ul class="flex flex-row flex-wrap">
           <li v-for="org in UNIQUE_ORGS" :key="org" class="h-8 w-1/2">
-            <input :id="org" type="checkbox" class="mr-3" />
+            <input :id="org" v-model="selectedOrgs" :value="org" type="checkbox" class="mr-3" />
             <label :for="org">{{ org }}</label>
           </li>
         </ul>
@@ -21,6 +21,11 @@ export default {
   name: 'JobFiltersSidebarOrganizations',
   components: {
     CollapsibleAccordion
+  },
+  data() {
+    return {
+      selectedOrgs: []
+    }
   },
   computed: {
     ...mapState(useJobsStore, [UNIQUE_ORGS])
