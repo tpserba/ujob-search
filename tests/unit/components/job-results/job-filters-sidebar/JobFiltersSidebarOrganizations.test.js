144646be35c01a23ce5b6b3a3d357a6008dfd5ab
Tomás Praga <tpraga@serbatic.es>
2023-05-29 15:39:22 +0200

    test that filtering is triggered by user interaction

--- tests/unit/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.test.js
+++ tests/unit/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import { createTestingPinia } from '@pinia/testing'
 import JobFiltersSidebarOrganizations from '@/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.vue'
-
+import { useUserStore } from '@/stores/user'
 import { useJobsStore } from '@/stores/jobs'
 
 describe('JobFiltersSidebarOrganizations', () => {
@@ -24,5 +24,29 @@ describe('JobFiltersSidebarOrganizations', () => {
     const orgsListItems = screen.getAllByRole('listitem')
     const orgs = orgsListItems.map((node) => node.textContent)
     expect(orgs).toEqual(['google', 'amazon'])
+  })
+
+  it('communicates that user has selected checkbox for organization', async () => {
+    const pinia = createTestingPinia()
+    const userStore = useUserStore()
+    const jobsStore = useJobsStore()
+    jobsStore.UNIQUE_ORGS = new Set(['google', 'amazon'])
+
+    render(JobFiltersSidebarOrganizations, {
+      global: {
+        plugins: [pinia],
+        stubs: {
+          FontAwesomeIcon: true
+        }
+      }
+    })
+    const button = screen.getByRole('button', { name: /organizations/i })
+    await userEvent.click(button)
+    const googleCheckbox = screen.getByRole('checkbox', {
+      name: /google/i
+    })
+    await userEvent.click(googleCheckbox)
+
+    expect(userStore.ADD_SELECTED_ORGS).toHaveBeenCalledWith(['google'])
   })
 })
