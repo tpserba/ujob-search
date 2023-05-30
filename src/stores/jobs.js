eb5bed32c261f37cfe8826f51a21b7308e319f7e
Tomás Praga <tpraga@serbatic.es>
2023-05-30 10:10:31 +0200

    create computed property for multiple filters

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -4,6 +4,7 @@ export const FETCH_JOBS = 'FETCH_JOBS'
 export const UNIQUE_ORGS = 'UNIQUE_ORGS'
 import { useUserStore } from '@/stores/user'
 export const UNIQUE_JOB_TYPES = 'UNIQUE_JOB_TYPES'
+export const FILTERED_JOBS = 'FILTERED_JOBS'
 export const FILTERED_JOBS_BY_ORGS = 'FILTERED_JOBS_BY_ORGS'
 export const FILTERED_JOBS_BY_JOB_TYPES = 'FILTERED_JOBS_BY_JOB_TYPES'
 
@@ -40,6 +41,17 @@ export const useJobsStore = defineStore('jobs', {
         return state.jobs
       }
       return state.jobs.filter((job) => userStore.selectedJobTypes.includes(job.jobType))
+    },
+    [FILTERED_JOBS](state) {
+      const userStore = useUserStore()
+      const noSelectedOrgs = userStore.selectedOrgs.length === 0
+      const noSelectedJobTypes = userStore.selectedJobTypes === 0
+      if (noSelectedOrgs && noSelectedJobTypes) {
+        return state.jobs
+      }
+      return state.jobs
+        .filter((job) => userStore.selectedOrgs.includes(job.organization))
+        .filter((job) => userStore.selectedJobTypes.includes(job.jobType))
     }
   }
 })
