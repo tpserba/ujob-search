376efc4f3ffe02d3ae996b23c462e614c41ef0a5
Tomás Praga <tpraga@serbatic.es>
2023-05-31 11:45:24 +0200

    create tests for decoupled checkbouxgroup component

--- tests/unit/components/job-results/job-filters-sidebar/JobFiltersSidebarCheckboxGroup.test.js
+++ tests/unit/components/job-results/job-filters-sidebar/JobFiltersSidebarCheckboxGroup.test.js
@@ -1,20 +1,26 @@
 import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import { createTestingPinia } from '@pinia/testing'
-import JobFiltersSidebarJobTypes from '@/components/job-results/job-filters-sidebar/JobFiltersSidebarJobTypes.vue'
-import { useUserStore } from '@/stores/user'
-import { useJobsStore } from '@/stores/jobs'
+import JobFiltersSidebarCheckboxGroup from '@/components/job-results/job-filters-sidebar/JobFiltersSidebarCheckboxGroup.vue'
+
 import { useRouter } from 'vue-router'
 import { vi } from 'vitest'
 
 vi.mock('vue-router')
-describe('JobFiltersSidebarJobTypes', () => {
-  const renderJobFiltersSidebarJobTypes = () => {
-    const pinia = createTestingPinia()
-    const jobsStore = useJobsStore()
-    const userStore = useUserStore()
 
-    render(JobFiltersSidebarJobTypes, {
+describe('JobFiltersSidebarCheckboxGroup', () => {
+  const createProps = (props = {}) => ({
+    header: 'Some header',
+    uniqueValues: new Set(['value a', 'value b']),
+    action: vi.fn(),
+    ...props
+  })
+  const renderJobFiltersSidebarCheckboxGroup = (props) => {
+    const pinia = createTestingPinia()
+    render(JobFiltersSidebarCheckboxGroup, {
+      props: {
+        ...props
+      },
       global: {
         plugins: [pinia],
         stubs: {
@@ -22,43 +28,57 @@ describe('JobFiltersSidebarJobTypes', () => {
         }
       }
     })
-    return { jobsStore, userStore }
   }
-  it('renders a unique list of job types from jobs', async () => {
-    const { jobsStore } = renderJobFiltersSidebarJobTypes()
-    jobsStore.UNIQUE_JOB_TYPES = new Set(['full-time', 'part-time'])
+  it('renders a unique list of values', async () => {
+    const props = createProps({
+      header: 'Job Types',
+      uniqueValues: new Set(['full-time', 'part-time'])
+    })
+
+    renderJobFiltersSidebarCheckboxGroup(props)
 
     const button = screen.getByRole('button', { name: /job types/i })
     await userEvent.click(button)
+
     const jobTypesListItems = screen.getAllByRole('listitem')
     const jobTypes = jobTypesListItems.map((node) => node.textContent)
     expect(jobTypes).toEqual(['full-time', 'part-time'])
   })
 
   describe('when user clicks checkbox', () => {
-    it('communicates that user has selected checkbox for job type', async () => {
+    it('communicates that user has selected checkbox for value', async () => {
       useRouter.mockReturnValue({ push: vi.fn() })
-      const { jobsStore, userStore } = renderJobFiltersSidebarJobTypes()
-      jobsStore.UNIQUE_JOB_TYPES = new Set(['full-time', 'part-time'])
+      const action = vi.fn()
+      const props = createProps({
+        header: 'Job Types',
+        uniqueValues: new Set(['full-time', 'part-time']),
+        action
+      })
+      renderJobFiltersSidebarCheckboxGroup(props)
 
       const button = screen.getByRole('button', { name: /job types/i })
       await userEvent.click(button)
+
       const fullTimeCheckbox = screen.getByRole('checkbox', {
         name: /full-time/i
       })
       await userEvent.click(fullTimeCheckbox)
 
-      expect(userStore.ADD_SELECTED_JOB_TYPES).toHaveBeenCalledWith(['full-time'])
+      expect(action).toHaveBeenCalledWith(['full-time'])
     })
 
     it('navigates user to JobsResults page to see fresh batch of filtered jobs', async () => {
       const push = vi.fn()
       useRouter.mockReturnValue({ push })
-      const { jobsStore } = renderJobFiltersSidebarJobTypes()
-      jobsStore.UNIQUE_JOB_TYPES = new Set(['full-time'])
+      const props = createProps({
+        header: 'Job Types',
+        uniqueValues: new Set(['full-time'])
+      })
+      renderJobFiltersSidebarCheckboxGroup(props)
 
       const button = screen.getByRole('button', { name: /job types/i })
       await userEvent.click(button)
+
       const fullTimeCheckbox = screen.getByRole('checkbox', {
         name: /full-time/i
       })
