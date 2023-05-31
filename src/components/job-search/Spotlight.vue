ff45a5dbaa58765b270119c0d64bde62f4251c3c
Tomás Praga <tpraga@serbatic.es>
2023-05-31 11:56:11 +0200

    refactor landing page cards to vue3 syntax

--- src/components/job-search/Spotlight.vue
+++ src/components/job-search/Spotlight.vue
@@ -10,20 +10,15 @@
   </ul>
 </template>
 
-<script>
+<script setup>
 import axios from 'axios'
-export default {
-  name: 'Spotlight',
-  data() {
-    return {
-      spotlights: []
-    }
-  },
-  async mounted() {
-    const baseURL = import.meta.env.VITE_APP_API_URL
-    const url = `${baseURL}/spotlights`
-    const response = await axios.get(url)
-    this.spotlights = response.data
-  }
+import { computed, onMounted, ref } from 'vue'
+const spotlights = ref([])
+const getSpotlights = async () => {
+  const baseURL = import.meta.env.VITE_APP_API_URL
+  const url = `${baseURL}/spotlights`
+  const response = await axios.get(url)
+  spotlights.value = response.data
 }
+onMounted(getSpotlights)
 </script>
