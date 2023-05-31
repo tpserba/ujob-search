71ea70383aad27482b9de5790704b869f081a403
Tomás Praga <tpraga@serbatic.es>
2023-05-31 10:29:30 +0200

    create reusable checkboxGroup component for filtering

---
+++ src/components/job-results/job-filters-sidebar/JobFilterSidebarCheckboxGroup.vue
@@ -0,0 +1,48 @@
+<template>
+  <CollapsibleAccordion :header="header">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li v-for="value in uniqueValues" :key="value" class="h-8 w-1/2">
+            <input
+              :id="value"
+              v-model="selectedValues"
+              @change="selectValue"
+              :value="value"
+              type="checkbox"
+              class="mr-3"
+            />
+            <label :for="jobType">{{ jobType }}</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </CollapsibleAccordion>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue'
+const props = defineProps({
+  header: {
+    type: String,
+    required: true
+  },
+  uniqueValues: {
+    type: Set,
+    required: true
+  },
+  action: {
+    type: Function,
+    required: true
+  }
+})
+const selectedValues = ref([])
+
+const router = useRouter()
+const selectValue = () => {
+  props.action(selectedValues.value)
+  router.push({ name: 'JobsResults' })
+}
+</script>
