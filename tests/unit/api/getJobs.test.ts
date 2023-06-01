ef34afcebbc97b4bc6b94c270a8e07cfcd2a67ed
Tomás Praga <tpraga@serbatic.es>
2023-06-01 14:06:35 +0200

    tell ts that test is using mocked axios

--- tests/unit/api/getJobs.test.ts
+++ tests/unit/api/getJobs.test.ts
@@ -1,10 +1,13 @@
+import type { Mock } from 'vitest'
 import axios from 'axios'
-import getJobs from '@/api/getJobs.js'
+import getJobs from '@/api/getJobs.ts'
 
 vi.mock('axios')
+const axiosGetMock = axios.get as Mock
+
 describe('getJobs', () => {
   beforeEach(() => {
-    axios.get.mockResolvedValue({
+    axiosGetMock.mockResolvedValue({
       data: [
         {
           id: 1,
