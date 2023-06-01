147f468fc3797c5fa7e58f10000166c8d5af6a2f
Tomás Praga <tpraga@serbatic.es>
2023-06-01 13:57:27 +0200

    update getJobs api to ts, and create ts config to handle global envs

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
-
+import type { Job } from '@/api/types'
 const getJobs = async () => {
   const baseURL = import.meta.env.VITE_APP_API_URL
   const url = `${baseURL}/jobs`
-  const response = await axios.get(url)
+  const response = await axios.get<Job[]>(url)
   return response.data
 }
 
