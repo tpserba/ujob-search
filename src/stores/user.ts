85e99eca293ec1045e267dbf94d29b6f4af7d251
Tomás Praga <tpraga@serbatic.es>
2023-06-01 15:21:33 +0200

    define types for user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,8 +1,14 @@
 import { defineStore } from 'pinia'
 export const ADD_SELECTED_ORGS = 'ADD_SELECTED_ORGS'
 export const ADD_SELECTED_JOB_TYPES = 'ADD_SELECTED_JOB_TYPES'
+export interface UserState {
+  isLoggedIn: boolean
+  selectedOrgs: string[]
+  selectedJobTypes: string[]
+}
+
 export const useUserStore = defineStore('user', {
-  state: () => ({
+  state: (): UserState => ({
     isLoggedIn: false,
     selectedOrgs: [],
     selectedJobTypes: []
@@ -11,10 +17,10 @@ export const useUserStore = defineStore('user', {
     loginUser() {
       this.isLoggedIn = true
     },
-    [ADD_SELECTED_ORGS](orgs) {
+    [ADD_SELECTED_ORGS](orgs: string[]) {
       this.selectedOrgs = orgs
     },
-    [ADD_SELECTED_JOB_TYPES](jobTypes) {
+    [ADD_SELECTED_JOB_TYPES](jobTypes: string[]) {
       this.selectedJobTypes = jobTypes
     }
   }
